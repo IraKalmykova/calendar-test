ea1340d727daceaaa6aadde6f584e104937cce4e
Ira <kalmykova_ira@ukr.net>
2019-07-31 13:36:49 +0300

    Updates

---
+++ precache-manifest.ef3264fd4c6e174a5fb91e7b20d680db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8e7d5d1e4abdb3e14df8728578184f",
+    "url": "/calendar-test/index.html"
+  },
+  {
+    "revision": "13d96b9a1f4cf2c1f288",
+    "url": "/calendar-test/static/css/2.2b08f868.chunk.css"
+  },
+  {
+    "revision": "38497196a23231c199c9",
+    "url": "/calendar-test/static/css/main.0ab49277.chunk.css"
+  },
+  {
+    "revision": "13d96b9a1f4cf2c1f288",
+    "url": "/calendar-test/static/js/2.bee2a5ef.chunk.js"
+  },
+  {
+    "revision": "38497196a23231c199c9",
+    "url": "/calendar-test/static/js/main.55d72330.chunk.js"
+  },
+  {
+    "revision": "acf9039db605b9063184",
+    "url": "/calendar-test/static/js/runtime~main.8c1dfeb7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ba3a5f5d2344710a865c9d5adf20dbe.js"
+  "/calendar-test/precache-manifest.ef3264fd4c6e174a5fb91e7b20d680db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
